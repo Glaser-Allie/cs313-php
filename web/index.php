ebb714919a17e9596d1fa9a57fa21217351bc984
Glaser-Allie <gla13006@byui.edu>
2018-09-29 19:02:53 -0500

    update to index

--- web/index.php
+++ web/index.php
@@ -3,9 +3,12 @@ session_start();
 
 require("layout.php");
 
-// View all products
-else {
-	// Display site links
+
+// **DISPLAY PAGE**
+echo $header;
+
+echo $message;
+
 	echo "<a href='./index.php' class='breadcrumb'>Shoppe Home</a>";
 	
 	echo "<h2>Hoenn Region Pok&eacute;mon</h2>";
