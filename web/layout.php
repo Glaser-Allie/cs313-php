3d0f5d8cf9838b8b87186b8cc8c633bbb58d84a9
Glaser-Allie <gla13006@byui.edu>
2018-09-29 19:30:00 -0500

    edit layout

--- web/layout.php
+++ web/layout.php
@@ -11,7 +11,7 @@ $header = <<<HTML
 	<div class="container" ><a href='./index.php'><h1>Allie takes CS 313</h1></a></div>
 		
         <header>
-            <a href='./index.php' class="assignments">Assignments</a>
+            <a href='./index.php' class="assignments">Home<br /></a>
             <a href='./index.php?assignments=1' class="assignments">Assignments</a>
         
 		<div class='content'>
