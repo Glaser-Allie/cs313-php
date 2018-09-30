83ed0522a0d6f8e6aee7349af39e4c49f136fca8
Glaser-Allie <gla13006@byui.edu>
2018-09-29 19:35:56 -0500

    edits

--- web/index.php
+++ web/index.php
@@ -3,12 +3,10 @@ session_start();
 
 require("layout.php");
 
-// **DISPLAY PAGE**
-echo $header;
-
-// Checkout
+// Assignments
 if(isset($_GET['assignments'])) {
-	// Display site links
+	
+    // Display Assignment Links
     
     echo "<p>Coming Soon!<br /></p>";
 	
@@ -17,11 +15,8 @@ if(isset($_GET['assignments'])) {
 // View home page
 else {
 	// Display site links
-	echo "<a href='./index.php' class='breadcrumb'>Home</a>";
 	
 	echo "<h2>Follow along as I figure out how the internet works</h2>";
-
-	// Loop to display all products
 }
 
 echo $footer;
