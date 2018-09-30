3263d5f9cf6b9695f6d2b33663cb91f0d64de13b
Glaser-Allie <gla13006@byui.edu>
2018-09-29 19:05:11 -0500

    edit index

--- web/index.php
+++ web/index.php
@@ -13,17 +13,6 @@ echo $message;
 	
 	echo "<h2>Hoenn Region Pok&eacute;mon</h2>";
 
-	// Loop to display all products
-	foreach($products as $id => $product) {
-		echo "<div class='pokecard'>
-			<div><h4><a href='./index.php?view_product=$id'>" . $product['name'] . "</h4></div>
-            <div>" . $product['image'] . "</div></a>
-			<div class='card_description'>" . $product['type'] . "\n\n|\n\n" . $product['wave'] . "\n Wave</div>
-            <div>$" . $product['price'] . "</div>            
-		</div>";
-	}
-}
-
 echo $footer;
 
 ?>
