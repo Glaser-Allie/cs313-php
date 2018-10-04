d0788e9acd11df9b077a9aaf6b3b5b7a76ffe169
Glaser-Allie <gla13006@byui.edu>
2018-10-04 18:25:49 -0500

    refined styles

--- web/layout.php
+++ web/layout.php
@@ -9,7 +9,7 @@ $header = <<<HTML
     <meta name='viewport' content='width=device-width, initial-scale=1' />
     <!--iOS-->
     <meta name="apple-mobile-web-app-capable" content="yes">
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
     <!--Android-->
     <meta name="mobile-web-app-capable" content="yes">
 </head>
