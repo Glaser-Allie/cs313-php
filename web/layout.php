d12362213e0522348d3c87957fcce78117d92e31
Glaser-Allie <gla13006@byui.edu>
2018-10-05 13:12:26 -0500

    add store

--- web/layout.php
+++ web/layout.php
@@ -10,15 +10,20 @@ $header = <<<HTML
     <!--iOS-->
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <link rel="apple-touch-icon" href="touch-icon-iphone.png">
+<link rel="apple-touch-icon" sizes="152x152" href="touch-icon-ipad.png">
+<link rel="apple-touch-icon" sizes="180x180" href="touch-icon-iphone-retina.png">
+<link rel="apple-touch-icon" sizes="167x167" href="touch-icon-ipad-retina.png">
     <!--Android-->
     <meta name="mobile-web-app-capable" content="yes">
     <link rel="manifest" href="./maifest.json">
+    
 </head>
 <body>
     <header>
         <nav>
             <ul>
-                <li><a href='http://cs313.allieglaser.com/'>Home</a></li>
+                <li><a href='./index.php'>Home</a></li>
                 <li><a href='./index.php?assignments=1'>Assignments</a></li>
             </ul>
         </nav>
