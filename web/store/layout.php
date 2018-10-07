c7326b311d0094f6110caa1e1b23308b99fd83a7
Glaser-Allie <gla13006@byui.edu>
2018-10-07 16:34:53 -0500

    still tryin' to fix this store

--- web/store/layout.php
+++ web/store/layout.php
@@ -6,6 +6,7 @@ $header = <<<HTML
 <head>
 	<title>SnoGourmet</title>
 	<link rel='stylesheet' type='text/css' href='./style.css' />
+    <meta content="width=device-width, initial-scale=1" name="viewport" />
 </head>
 <body>
 	<div class="container" >
